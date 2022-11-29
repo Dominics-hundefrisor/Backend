e6732555048c8fa624d5d6f9cb6b059e102bd751
Battals <batt0006@stud.kea.dk>
2022-11-29 11:26:24 +0100

    Initial commit

---
+++ src/test/java/com/example/hf/HfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
